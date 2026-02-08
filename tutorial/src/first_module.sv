6745737719c6c4d0038f47d77b1fbf68598ba7b5
Victor Geraldo <vgeraldo@purdue.edu>
2026-02-08 16:44:28 -0500

    Commented out Dataflow and Behavioral models

--- tutorial/src/first_module.sv
+++ tutorial/src/first_module.sv
@@ -8,13 +8,13 @@ module first_module(
     ha u1 (.a(input_A), .b(input_B), .sum(output_S), .cout(output_C));
 
     // Dataflow Modeling:
-    assign output_S = input_A ^ input_B;
-    assign output_C = input_A & input_B;
+    // assign output_S = input_A ^ input_B;
+    // assign output_C = input_A & input_B;
 
     // Behavioral Modeling:
-    always_comb begin
-        {output_C, output_S} = input_A + input_B;
-    end
+    // always_comb begin
+        // {output_C, output_S} = input_A + input_B;
+    // end
 
 endmodule
 
