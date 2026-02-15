2e1e78e891fa00ca054041c5808a2c29d6e547b8
Victor Geraldo <vgeraldo@purdue.edu>
2026-02-15 16:44:46 -0500

    Fix sum bit in full_adder

--- digital/lab1/src/full_adder.sv
+++ digital/lab1/src/full_adder.sv
@@ -8,7 +8,7 @@ module full_adder(
     // Note: an alternative way of expressing this would be:
     // assign {cout, s} = a + b + cin;
     // TODO: There's a bug in here!
-    assign s = a ^ b;
+    assign s = a ^ b ^ cin;
     assign cout = (a & b) | ((a ^ b) & cin);
 
 endmodule
